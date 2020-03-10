1199c4ab66324171e75298d388cc5063575278dd
xAlisbeth <60382185+xAlisbeth@users.noreply.github.com>
2020-03-10 11:56:46 +0100

    Update Fractale.cs

--- Fractale.cs
+++ Fractale.cs
@@ -8,18 +8,19 @@ namespace PROJET_INFO_PUGET_Camille_PUVIKARAN_Thanujan
 {
     class Fractale
     {
+        // attributes
         Pixel2[,] matrice;
         
-
+        // constructor
         public Fractale()
         {
             this.matrice =Attribution();
                 
         }
+        
         public Pixel2[,] Attribution()
         {
             Pixel2[,] m = new Pixel2[1000,1000];
-
             return m;
         }
     }
